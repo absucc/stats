62dd79535860db62122fa6ad22d4b9858f073da6
RemiixInc <39117916+RemiixInc@users.noreply.github.com>
2020-12-01 18:14:49 +1100

    Update youtube.js

--- _data/youtube.js
+++ _data/youtube.js
@@ -1,5 +1,5 @@
 var request = require("sync-request");
-var key = "AIzaSyCUWPRUQYHaqKub36FNPHIi6uADh3F-KjQ"
+var key = process.env.API_KEY
 // make the request
 var res = request("GET", `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCkuKzwbQX0WFG2v457QTOSw&key=${key}`,{
   headers: {
